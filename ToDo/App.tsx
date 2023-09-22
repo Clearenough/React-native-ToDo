ca68a1210c84250bfa07bf5fa192c222bd486efc
Clearenough <smogida1337@gmail.com>
2023-09-22 18:20:21 +0300

    init project and create slice for tasks and store

---
+++ ToDo/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+function App(): JSX.Element {
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ ToDo/src/store/slices/tasksSlice.ts
@@ -0,0 +1,42 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface Task {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+}
+
+type TasksState = Array<Task>;
+
+const initialState: TasksState = [];
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state: TasksState, action: PayloadAction<Task>) => {
+      state.push(action.payload);
+    },
+    deleteTask: (state: TasksState, action: PayloadAction<number>) => {
+      const id = action.payload;
+      state.filter(task => id !== task.id);
+    },
+    updateTask: (state: TasksState, action: PayloadAction<Task>) => {
+      const {id, text} = action.payload;
+      const updatedTask = state.find(task => task.id === id);
+      if (updatedTask) {
+        updatedTask.text = text;
+      }
+    },
+    completeTask: (state: TasksState, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const completedTask = state.find(task => task.id === id);
+      if (completedTask) {
+        completedTask.isCompleted = true;
+      }
+    },
+  },
+});
+
+export const {addTask, deleteTask, updateTask, completeTask} =
+  tasksSlice.actions;

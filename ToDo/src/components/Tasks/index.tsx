6b3756b35e2f0bb3d2b23476b73a957208f7f47e
Clearenough <smogida1337@gmail.com>
2023-10-04 16:06:18 +0300

    feat: optimize FlatList

--- ToDo/src/components/Tasks/index.tsx
+++ ToDo/src/components/Tasks/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {
   FlatList,
   KeyboardAvoidingView,
+  ListRenderItemInfo,
   Platform,
   StyleSheet,
   View,
 } from 'react-native';
 import {useAppSelector} from '../../hooks/hooks';
 import {selectTasks} from '../../store/slices/tasksSlice';
+import {ITask} from '../../types/common';
 import Task from '../Task';
 
 function Tasks() {
   const tasks = useAppSelector(selectTasks);
+
+  const renderItem = useCallback(
+    ({item}: ListRenderItemInfo<ITask>) => <Task task={item} />,
+    [],
+  );
 
   return (
     <View style={styles.container}>
@@ -20,8 +27,9 @@ function Tasks() {
         behavior="position">
         <FlatList
           data={tasks}
-          renderItem={task => <Task task={task.item} />}
-          scrollEnabled={true}
+          renderItem={renderItem}
+          scrollEnabled={Platform.OS === 'ios' && true}
+          keyExtractor={item => item.id}
         />
       </KeyboardAvoidingView>
     </View>
